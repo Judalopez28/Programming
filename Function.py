1f4ac37116e9a9cc868e066f0164b1316d3f2a18
Judalopez28 <lopez.juandavid28@gmail.com>
2024-01-22 14:06:07 -0700

    First Upload test

---
+++ Function.py
@@ -0,0 +1,9 @@
+def main():
+    Input = getInput()
+    print(Input)
+
+def getInput():
+    toShow = input("Tell me what you would like to show")
+    return (toShow)
+
+main()
---
+++ Input_Text.py
@@ -0,0 +1,2 @@
+Print_This = input("Enter what you want to print")
+print(Print_This)
